8b8e241922537a1d08ab6aaae0463d5ab3bff60d
Felipe Schreiber Fernandes <schreiber.felipe@gmail.com>
2023-01-22 23:36:25 -0300

    var to string

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = local.aws_region
+  region = "us-east-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "terraform-state-felipe"
     key = "state/edc/mod1/terraform.tfstate"
-    region = local.aws_region
+    region = "us-east-2"
   }
 }
