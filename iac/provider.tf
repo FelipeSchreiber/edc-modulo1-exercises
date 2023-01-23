ad64df2430fa0392b742e8b2008d1656a7d0b480
Felipe Schreiber Fernandes <schreiber.felipe@gmail.com>
2023-01-22 21:04:55 -0300

    Split terraform files

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,34 +1,11 @@
-resource "aws_s3_bucket" "datalake" {
-    bucket = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}"
-    acl = "private"
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
-      }
-    }
-  tags = {
-    CURSO = "EDC"
-  }
-}
-
-resource "aws_s3_bucket_object" "codigo_spark" {
-  bucket = aws_s3_bucket.datalake.id
-  key = "emr-code/pyspark/job_spark_from_tf.py"
-  acl = "private"
-  source = "../job_spark.py"
-  etag = filemd5("../job_spark.py")
-}
-
 provider "aws" {
-  region = "us-east-2"
+  region = var.aws_region
 }
 
 terraform {
   backend "s3" {
     bucket = "terraform-state-felipe"
     key = "state/edc/mod1/terraform.tfstate"
-    region = "us-east-2"
+    region = var.aws_region
   }
 }
